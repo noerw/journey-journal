4eb6c8d14a45d1b954804abe8767e5670e776fd2
noerw <noerw@gmx.de>
2015-07-29 22:14:26 +0200

    map with sidebar

--- webapp/js/mapInit.js
+++ webapp/js/mapInit.js
@@ -1,7 +1,7 @@
 /**  
 * @desc   Creates a map in  the HTML document using leaflet
-* @author Norwin Roosen, Stefanie Nagelsdiek
-* @date   150601
+* @author Norwin Roosen
+* @date   150822
 */
 
 'use strict';
@@ -10,52 +10,24 @@
 JL("mapLogger").setOptions({"appenders": [consoleAppender]});
 
 // create a new map, setting the view to Muenster
-var map = L.map('map').setView([51.96, 7.61], 13);
+var map = L.map('map').setView([51.96, 7.624], 14);
 
-// define layers
-// OpenStreetMap basemap
+// define basemaps
 var osmLayer = new L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 });
-
-// OpenCycleMap basemap
-var ocmLayer = new L.tileLayer('http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', {
-     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors, Tiles courtesy of <a href="http://www.thunderforest.com/">Andy Allan</a>'
-});
-
-
-// Initialise the FeatureGroup to store database layers
-var dbFeatures = new L.FeatureGroup();
-map.addLayer(dbFeatures);
-
+var tonerLayer = new L.StamenTileLayer("toner");
+var watercolorLayer = new L.StamenTileLayer("watercolor");
 
 // layer group for basemaps
 var baseMaps = {
-     "OpenStreetMap": osmLayer,
-     "OpenCycleMap": ocmLayer
+     "Open Street Map": osmLayer,
+     "Stamen Toner": tonerLayer,
+     "Stamen Watercolor": watercolorLayer,
 }
 
-// layer control switch
+// layer control switch with basemaps
 var layerControl = new L.control.layers(baseMaps).addTo(map);
 
 // sets the OSM layer as default
-osmLayer.addTo(map);
-
-// add a marker at the position of the GEO building.
-// marker popup contains the image img/ifgi.jpg
-
-// custom icon for the marker
-var greenMarkerIcon = L.icon({
-    iconUrl:   'lib/css/images/marker-icon-green.png',
-    shadowUrl: 'lib/css/images/marker-shadow.png',
-	iconSize:    [25, 41],
-	iconAnchor:  [12, 41],
-	popupAnchor: [1, -34],
-	shadowSize:  [41, 41]
-});
-
-L.marker([51.9694, 7.5957], {icon: greenMarkerIcon}).addTo(map)
-        .bindPopup("<b>IFGI</b><br>Heisenbergstr. 2<br>48149 Münster<br>" +
-        "<a href=\"http://ifgi.de/\">" +
-        "<img width=\"300\" src=\"img/ifgi.jpg\" alt=\"picture of IFGI\" >" +
-        "</a>");
+watercolorLayer.addTo(map);
